0b2487e1ca19227d2022e44a7b18b67e1998fcf6
amadansi26-cmyk <amadansi26@bentleyschool.org>
2026-01-19 10:52:44 -0800

    Update CSP_5_01_Searching.py

--- CSP_5_01_Searching.py
+++ CSP_5_01_Searching.py
@@ -1,19 +1,45 @@
+def randomSearch(items: list, target) -> int:
+    import random
 
-def randomSearch(items:list, target) -> int:
-    #Modify the below function such that it takes in a list of items and a target value.
-    #Randomly choose an item from the list and if it isn't the target value try again.
-    #print out the amount of tries it took and return the index of the target value
-    pass
+    attempts = 0
 
-def linearSearch(items:list, target) ->tuple[int,int]:
-    #Modify the below function such that it implements linear search.
-    #Return the index of the target value and the amount of checks it took
-    #if the value is not within the list return -1 as the index.
-    pass
+    while True:
+        attempts += 1
+        random_index = random.randint(0, len(items) - 1)
+
+        if items[random_index] == target:
+            print(f"Target was found after {attempts} tries")
+            return random_index
+
+def linearSearch(items: list, target):
+    index = 0
+    checks = 0
+
+    while index < len(items):
+        checks += 1
+
+        if items[index] == target:
+            return index, checks
+
+        index += 1
+
+    return -1, checks
 
 
-def binarySearch(items:list, target) -> tuple[int,int]:
-    # Modify the below function such that it implements linear search.
-    # Return the index of the target value and the amount of checks it took
-    # if the value is not within the list return -1 as the index.
-    pass
+def binarySearch(items: list, target):
+    left = 0
+    right = len(items) - 1
+    checks = 0
+
+    while left <= right:
+        checks += 1
+        middle = (left + right) // 2
+
+        if items[middle] == target:
+            return middle, checks
+        elif items[middle] < target:
+            left = middle + 1
+        else:
+            right = middle - 1
+
+    return -1, checks
